2b8a888e416211612b6f3f8a2443b3398dec8712
Lindsen13 <ivo.lindsen@hotmail.com>
2022-10-18 14:52:04 +0200

    formatting change

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 import glob
 import os
 
- 
+
 def read_temperature() -> float:
     if os.environ.get('local') == 'true':
         # Return -1 when we're developing locally.
@@ -11,9 +11,7 @@ def read_temperature() -> float:
         # Return temperature fetched from sensor.
         os.system('modprobe w1-gpio')
         os.system('modprobe w1-therm')
-    
         file_path = f"{glob.glob('/sys/bus/w1/devices/' + '28*')[0]}/w1_slave"
-
         with open(file_path, 'r') as f:
             data = f.read()
         temperature = int(data.split('t=')[-1].strip()) / 1000
